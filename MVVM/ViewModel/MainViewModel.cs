4fddb0b3c266a430032a1d4ae5d59c9542e00019
bseckin <berkay.seckin1@gmail.com>
2017-10-11 21:00:52 +0200

    Tick added

--- MVVM/ViewModel/MainViewModel.cs
+++ MVVM/ViewModel/MainViewModel.cs
@@ -3,6 +3,7 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
+using System.Windows.Threading;
 
 namespace MVVM.ViewModel
 {
@@ -15,6 +16,17 @@ namespace MVVM.ViewModel
         {
             Shapes = new List<string>() { "Rechteck", "Kreis", "Quadrat", "Parallelogram", "Rechtwinkeliges Dreieck"};
             SelectedItem = "Parallelogram";
+
+            DispatcherTimer t = new DispatcherTimer();
+            t.Interval = new TimeSpan(0, 0, 3);
+            t.Tick += T_Tick;
+            t.Start();
+
+        }
+
+        private void T_Tick(object sender, EventArgs e)
+        {
+            
         }
     }
 }
