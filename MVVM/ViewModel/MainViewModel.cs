4ba35644602741f63391220eaf7c3c7a31c3e35a
Berkay Seckin <wi16b031@TECHNIKUM-WIEN.AT>
2017-10-11 20:55:24 +0200

    Projektdateien hinzufügen.

---
+++ MVVM/ViewModel/MainViewModel.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace MVVM.ViewModel
+{
+    class MainViewModel
+    {
+        public List<string> Shapes { get; set; }
+
+        public string SelectedItem { get; set; }
+        public MainViewModel()
+        {
+            Shapes = new List<string>() { "Rechteck", "Kreis", "Quadrat", "Parallelogram", "Rechtwinkeliges Dreieck"};
+            SelectedItem = "Parallelogram";
+        }
+    }
+}
